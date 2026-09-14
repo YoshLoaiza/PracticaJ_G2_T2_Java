3fdf27c9c3c604b568aaee299c4b312f5fac9708
Joss <Joss@JOSS>
2026-09-13 22:33:12 -0600

    Versión_01: Proyecto vacío.

---
+++ PracticaJ_G2_T2_Java/src/practicaj_g2_t2_java/PracticaJ_G2_T2_Java.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practicaj_g2_t2_java;
+
+/**
+ *
+ * @author joss
+ */
+public class PracticaJ_G2_T2_Java {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+    }
+    
+}
